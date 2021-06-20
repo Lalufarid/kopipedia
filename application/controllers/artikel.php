751c9a4a8570e5435d9fe11094d4d8dd7343656b
Lalu Farid <lalufarid18@gmail.com>
2021-06-20 23:14:31 +0800

    Initial Commit

---
+++ application/controllers/artikel.php
@@ -0,0 +1,21 @@
+<?php 
+
+// class Artikel extends CI_Controller {
+// 	public function tulisan()
+// 	{
+// 		$this->load->model('m_artikel');
+// 		$data['artikel'] = $this->m_artikel->tampil_data();
+
+
+// 		$this->load->view('templates/header_artikel');
+// 		$this->load->view('artikel',$data);
+// 		$this->load->view('templates/footer_artikel');
+
+
+// 	}
+
+	
+
+}
+
+ ?>
